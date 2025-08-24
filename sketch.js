477e1f1ba02dbcac3c6450b35f9a099bc419a0a6
Shahadat <saaadmolla@gmail.com>
2025-08-25 03:08:07 +0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   sketch.js

--- sketch.js
+++ sketch.js
@@ -35,22 +35,25 @@ function draw() {
   lineXone = random(0,width);
   lineYone = random(0,height/2);
   //hills
-  stroke(0);
+  stroke(50);
   fill(80);
   triangle(-40,500, 175,200, 550,500);
 //cloud
+stroke(255);
 fill(255);
+
   ellipse(cloudOneX - 40, 100, 60, 20);
-//hill  
+//hill 
+stroke(50); 
 fill(80);
   triangle(200,500, 500,50, 800,500);
-
+  stroke('rgba(42, 51, 42, 0.56)');
   fill('rgb(50,76,50)');
   rect(0,500,1980,720);
 
   //clouds
   
-
+  stroke(255);
   fill(255);
   ellipse(cloudOneX,50,80,40);
   
@@ -58,16 +61,21 @@ fill(80);
 
   //growing tree
 //trunk
+stroke('rgba(66, 51, 47, 0.7)')
 fill("rgb(118,80,72)");
 rect(80, 270, 25, 350);
 //leaves
+stroke('rgba(68, 99, 80, 0.25)')
 fill("green");
 triangle(20, 370, 85, 100, 170, 370);
 
 //trunk
+stroke('rgba(66, 51, 47, 0.7)');
 fill("rgb(118,80,72)");
 rect(460, 440, 23, 200);
+
 //leaves
+stroke('rgba(50, 87, 65, 0.25)');
 fill("green");
 triangle(350, 540, 470, 240 - (frameCount % 280)/2, 580, 540);
 
