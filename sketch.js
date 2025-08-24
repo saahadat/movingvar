9c55d43789eac50e803f77489f1c716c78b2df00
Shahadat <saaadmolla@gmail.com>
2025-08-25 02:54:56 +0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   sketch.js

--- sketch.js
+++ sketch.js
@@ -10,10 +10,13 @@ function setup() {
 
 function draw() {
   background('navy');
+  frameRate(15);
+
   
+
   // fill(0);
   // text(`${mouseX},${mouseY}`,20,20);
-  
+  stroke(0);
   fill(255);
   text(`mouseX:${mouseX},mouseY:${mouseY}\nframecount:${frameCount}`,20,20);
 
@@ -28,22 +31,29 @@ function draw() {
   // stroke("navy");
   // fill("navy");
   // circle(320,50,100);
-  
+  cloudOneX = frameCount % width
+  lineXone = random(0,width);
+  lineYone = random(0,height/2);
   //hills
   stroke(0);
   fill(80);
   triangle(-40,500, 175,200, 550,500);
+//cloud
+fill(255);
+  ellipse(cloudOneX - 40, 100, 60, 20);
+//hill  
+fill(80);
   triangle(200,500, 500,50, 800,500);
 
   fill('rgb(50,76,50)');
   rect(0,500,1980,720);
 
   //clouds
-  cloudOneX = frameCount % width
+  
 
   fill(255);
   ellipse(cloudOneX,50,80,40);
-  ellipse(cloudOneX - 40, 100, 60, 20);
+  
   ellipse(cloudOneX + 20, 150, 40, 10);
 
   //growing tree
@@ -59,6 +69,8 @@ fill("rgb(118,80,72)");
 rect(460, 440, 23, 200);
 //leaves
 fill("green");
-triangle(350, 540, 470, 240 - frameCount % 290, 580, 540);
+triangle(350, 540, 470, 240 - (frameCount % 280)/2, 580, 540);
 
+stroke("yellow");
+  line(lineXone,lineYone,lineXone+50,lineYone-50);
 }
